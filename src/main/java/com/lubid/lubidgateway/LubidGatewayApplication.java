9349e59a2b993ee804c22e918a90ecd9f3b3cd3f
dpound@naver.com <dpound@naver.com>
2024-02-18 18:37:56 +0900

    lubid project gateway 생성 및 뼈대 구현중

---
+++ src/main/java/com/lubid/lubidgateway/LubidGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lubid.lubidgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LubidGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LubidGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lubid/lubidgateway/LubidGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubid.lubidgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LubidGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
